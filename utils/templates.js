6fc2ba47660edfb1c73e3b708a73945759b2e5c4
Jerry Haagsma <haagsmag@gmail.com>
2016-05-06 00:16:23 -0400

    -Updated resolver to return robust link rather than iframe song embed

--- utils/templates.js
+++ utils/templates.js
@@ -1,6 +1,8 @@
 (function () {
   'use strict';
 
+  var _ = require('underscore');
+
   module.exports = {
     getSearchPromptTemplate: function () {
       return `<i>(enter a search term)</i>`;
@@ -28,7 +30,41 @@
     },
     // Returns full track link template
     getRobustTrackLinkTemplate: function (track) {
+      var result = '',
+          image,
+          artistName;
 
+      if (!track || !track.id) {
+        return result;
+      }
+
+      result += `<div style="display:inline-block;">`;
+      result += `<a href="https://embed.spotify.com/?uri=spotify:track:${track.id}" ` +
+        `target="_blank" style="text-decoration:none;color:#444;">`;
+      result += `<div style="display:flex;align-items:center;">`;
+
+      if (track.album && track.album.images && track.album.images.length) {
+        image = _.chain(track.album.images)
+          .sortBy('width')
+          .first()
+          .value();
+          
+        if (!!image) {
+          result += `<img src="${image.url}" alt="Album cover" ` +
+            `height="${image.height}" width="${image.width}" ` +
+            `style="margin-right:10px;" />`;
+        }
+      }
+
+      result += `<div>`;
+      result += `<div><b>${track.name || '(unknown song name)'}</b></div>`;
+
+      artistName = track.artists && track.artists.length ?
+        track.artists[0].name : '(unknown artist)';
+      result += `<div style="font-size:12px;margin-top:10px;">${artistName}</div>`;
+
+      result += `</div></div></a></div>`;
+      return result;
     }
   };
 }());
