fa2e4a7bb3b3ac8c6b0ff7458be110b68b1a3d6d
Jerry Haagsma <haagsmag@gmail.com>
2016-05-06 10:15:27 -0400

    -Fix styles for email-html safety

--- utils/templates.js
+++ utils/templates.js
@@ -38,25 +38,12 @@
         return result;
       }
 
-      result +=
-        `<style>` +
-          `.song-container {` +
-            `display: inline-block;` +
-            `padding: 15px;` +
-            `font-family: helvetica,sans-serif;` +
-            `border: 1px solid #2ebd59;` +
-            `border-radius: 3px;` +
-            `background-color: #000;` +
-          `}` +
-          `.song-container:hover {` +
-            `opacity: 0.8;` +
-          `}` +
-        `</style>`;
-
-      result += `<a href="https://embed.spotify.com/?uri=spotify:track:${track.id}" ` +
+      result += `<div><a href="https://embed.spotify.com/?uri=spotify:track:${track.id}" ` +
         `target="_blank" style="text-decoration:none;color:#fff;">`;
-      result += `<div class="song-container"`;
-      result += `<div style="display:flex;align-items:center;">`;
+      result += `<div style="display:inline-block;padding:15px;` +
+        `font-family:helvetica,sans-serif;border:1px solid #2ebd59;` +
+        `border-radius:3px;background-color:#000;">`;
+      result += `<div style="display:flex;">`;
 
       if (track.album && track.album.images && track.album.images.length) {
         image = _.chain(track.album.images)
@@ -64,21 +51,21 @@
           .first()
           .value();
 
-        if (!!image) {
+        if (!!image && !!image.url) {
           result += `<img src="${image.url}" alt="Album cover" ` +
-            `height="${image.height}" width="${image.width}" ` +
-            `style="margin-right:15px;" />`;
+            `height="64px" width="64px" style="margin-right:15px;" />`;
         }
       }
 
-      result += `<div>`;
-      result += `<div><b>${track.name || '(unknown song name)'}</b></div>`;
+      result += `<div style="margin-top:8px;">`;
+      result += `<div style="font-size:18px;"><b>` +
+        `${track.name || '(unknown song name)'}</b></div>`;
 
       artistName = track.artists && track.artists.length ?
         track.artists[0].name : '(unknown artist)';
       result += `<div style="font-size:12px;margin-top:10px;">${artistName}</div>`;
 
-      result += `</div></div></div></a>`;
+      result += `</div></div></div></a></div>`;
       return result;
     }
   };
